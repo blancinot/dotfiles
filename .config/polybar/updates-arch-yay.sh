4ba8ae0b5aa27f5db1b838e517e4adbf437a22fa
blancinot <benjamin.lancinot@reseau.eseo.fr>
2018-12-18 10:27:11 +0000

    updates-arch-yay.sh run with sudo and disable warning

--- .config/polybar/updates-arch-yay.sh
+++ .config/polybar/updates-arch-yay.sh
@@ -2,7 +2,7 @@
 # List all updates available from Yay (includes AUR)
 
 # Sync package list first
-yay -Sy > /dev/null
+sudo yay -Sy > /dev/null 2>&1
 
 # Get list and count of updates
 if ! updates=$(yay -Qu | wc -l); then
